411dd032872687718f99cb257fa22d056b95f09b
payamrastogi <payamrastogi@gmail.com>
2013-07-23 20:35:49 +0530

    ads table added

---
+++ Database/tbl_ads.sql
@@ -0,0 +1,16 @@
+create table ads 
+(
+	userid varchar(10),
+	company_name varchar(40),
+	location_from varchar(30),
+	location_to varchar(30),
+	car_model varchar(30),
+	registration_number varchar(30),
+	number_of_seats int,
+	seats_available int,
+	start_time timestamp,
+	return_time timestamp,
+	one_two_way int,
+	description varchar(100),
+	ad_status varchar(10)
+);
